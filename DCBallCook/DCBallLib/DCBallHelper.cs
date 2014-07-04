532e85616791fce2db1b0708cc7f023e92babad4
wozhengjl <zhengjianleia@163.com>
2014-07-04 22:16:05 +0800

    Detail DC Ball Report
    Detail DC Ball Report

---
+++ DCBallCook/DCBallLib/DCBallHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Double.Color.Ball.Common
+{
+    public static class DCBallHelper
+    {
+        public static string DecodeBalls(string balls)
+        {
+            IList<int> ballsNo = new List<int>();
+
+            for (int index = 0; index < balls.Length; index++) 
+            {
+                ballsNo.Add(DecodeBall(balls[index]));
+            }
+
+            return string.Join(",", ballsNo);
+        }
+
+        private static int DecodeBall(char ball)
+        {
+            if (ball >= 'A')
+            {
+                return ball - 'A' + 10;
+            }
+            else
+            {
+                return ball - '0';
+            }
+        }
+    }
+}
